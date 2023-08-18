06298b7e423d06ea18c6385b9c8f95270f0f118c
auipc <139733387+auipc@users.noreply.github.com>
2023-08-17 22:24:45 -0400

    more paging

--- kernel/mem/Paging.cpp
+++ kernel/mem/Paging.cpp
@@ -2,27 +2,36 @@
 #include <kernel/mem/Paging.h>
 #include <kernel/printk.h>
 
-extern "C" PageDirectoryEntry boot_page_directory[4096];
+extern "C" PageDirectory boot_page_directory;
+
+PageDirectory *PageDirectory::clone(PageDirectory *src) {
+	PageDirectory *dst = new PageDirectory();
+	for (int i = 0; i < 1024; i++) {
+		dst->entries[i].value = src->entries[i].value;
+	}
+	return dst;
+}
+
 
 void Paging::setup() {
 	int num_present = 0;
-	for (int i = 0; i < 4096; i++) {
-		assert(boot_page_directory[i].present <= 1);
-		if (boot_page_directory[i].present) {
+	for (int i = 0; i < 1024; i++) {
+		assert(boot_page_directory.entries[i].present <= 1);
+		if (boot_page_directory.entries[i].present) {
 			printk("Page directory entry %d is present\n", i);
 			// page_table_base
-			uint32_t page_table_base = boot_page_directory[i].page_table_base;
+			uint32_t page_table_base = boot_page_directory.entries[i].page_table_base;
 			printk("Page table base: %x\n", page_table_base & 0xfffff000u);
 			// page_table_base is a physical address, so we need to convert it
 			// to a virtual address
-			uint32_t *page_table = (uint32_t *)(page_table_base + 0xC0000000);
+			auto page_table = boot_page_directory.entries[i].get_page_table();
 			printk("page_table: %x\n", page_table);
-			/*for (int j = 0; j < 1024; j++) {
-				if (page_table[j] & 1) {
+
+			for (int j = 0; j < 1024; j++) {
+				if (page_table[j].present) {
 					printk("Page table entry %d is present\n", j);
 				}
-			}*/
-			num_present++;
+			}
 		}
 	}
 
--- kernel/mem/Paging.h
+++ kernel/mem/Paging.h
@@ -1,6 +1,24 @@
 #pragma once
 #include <kernel/stdint.h>
 
+union PageTableEntry {
+	struct {
+		uint32_t present : 1;
+		uint32_t read_write : 1;
+		uint32_t user_supervisor : 1;
+		uint32_t write_through : 1;
+		uint32_t cache_disabled : 1;
+		uint32_t accessed : 1;
+		uint32_t dirty : 1;
+		uint32_t page_table_attribute_index : 1;
+		uint32_t global : 1;
+		uint32_t ignored : 3;
+		uint32_t page_base : 20;
+	};
+
+	uint32_t value;
+} __attribute__((packed));
+
 union PageDirectoryEntry {
 	struct {
 		uint32_t present : 1;
@@ -16,6 +34,16 @@ union PageDirectoryEntry {
 	};
 
 	uint32_t value;
+
+	inline PageTableEntry *get_page_table() {
+		return (PageTableEntry *)((page_table_base << 12) + 0xC0000000);
+	}
+} __attribute__((packed));
+
+struct PageDirectory {
+	static PageDirectory* clone(PageDirectory* src);
+
+	PageDirectoryEntry entries[1024];
 } __attribute__((packed));
 
 class Paging {
