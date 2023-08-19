63c4f52a4a4c2a8bd495c819508df58db13bd103
auipc <139733387+auipc@users.noreply.github.com>
2023-08-19 01:57:46 -0400

    add todos

--- kernel/mem/Paging.cpp
+++ kernel/mem/Paging.cpp
@@ -19,13 +19,13 @@ PageTable *PageTable::clone() {
 	// page aligned
 	assert(reinterpret_cast<uint32_t>(dst) % PAGE_SIZE == 0);
 
-	// TODO find a good way to allocate pages
 	for (int i = 0; i < 1024; i++) {
 		if (!src->entries[i].present)
 			continue;
 		// just copy all values, what could go wrong.
 		dst->entries[i].value = src->entries[i].value;
-		//printk("src->entries[%d].value: %x\n", i, src->entries[i].value);
+
+		// TODO we need to be able to temporarily identity map this address so we can copy it's contents
 		printk("free page: %x\n", Paging::the()->m_allocator->find_free_page());
 	}
 
@@ -40,6 +40,7 @@ PageDirectory *PageDirectory::clone() {
 	assert(reinterpret_cast<uint32_t>(dst) % PAGE_SIZE == 0);
 
 	// TODO find a good way to allocate page tables
+	// TODO every page should share the kernels page tables, mostly for interrupts and kernel tasks.
 	for (int i = 0; i < 1024; i++) {
 		if (!src->entries[i].present)
 			continue;
