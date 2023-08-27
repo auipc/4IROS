2ecdf62cd1d40cdab0b920aad1e9cef98ca1e156
auipc <139733387+auipc@users.noreply.github.com>
2023-08-27 01:43:21 -0400

    add clang toolchain
    This doesn't compile because of some annoying errors regarding inline
    assembly. I should just move all the assembly code to their own
    respective files instead of trying to do weird hacks to get them working.

---
+++ cmake/toolchain-clang.cmake
@@ -0,0 +1,24 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME 4IROS)
+
+# which compilers to use for C and C++
+set(CMAKE_C_COMPILER   clang)
+set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_ASM_COMPILER clang)
+set(CMAKE_LINKER llvm-link)
+
+set(CMAKE_CXX_FLAGS "--target=i686-pc-none-elf -march=i686")
+set(CMAKE_C_FLAGS "--target=i686-pc-none-elf -march=i686")
+set(CMAKE_LINKER_FLAGS "--target=i686-pc-none-elf -march=i686")
+set(CMAKE_ASM_FLAGS "--target=i686-pc-none-elf -march=i686")
+
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER_WORKS 1)
+set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
+set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
