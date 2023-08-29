74f768d8e1eb27905524d7a4137b9daed18bc0d1
auipc <139733387+auipc@users.noreply.github.com>
2023-08-29 00:09:04 -0400

    add header

---
+++ kernel/multiboot.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <kernel/stdint.h>
+
+// https://www.gnu.org/software/grub/manual/multiboot/multiboot.html#multiboot_002eh
+struct MultiBootInfo 
+{
+	/* Multiboot magic */
+	uint32_t magic;
+	/* Multiboot info version number */
+	uint32_t flags;
+
+	/* Available memory from BIOS */
+	uint32_t mem_lower;
+	uint32_t mem_upper;
+
+	/* "root" partition */
+	uint32_t boot_device;
+
+	/* Kernel command line */
+	uint32_t cmdline;
+
+	/* Boot-Module list */
+	uint32_t mods_count;
+	uint32_t mods_addr;
+
+	/* Memory Mapping buffer */
+	uint32_t mmap_length;
+	uint32_t mmap_addr;
+
+	/* Drive Info buffer */
+	uint32_t drives_length;
+	uint32_t drives_addr;
+
+	/* ROM configuration table */
+	uint32_t config_table;
+
+	/* Boot Loader Name */
+	uint32_t boot_loader_name;
+
+	/* APM table */
+	uint32_t apm_table;
+
+	/* Video */
+	uint32_t vbe_control_info;
+	uint32_t vbe_mode_info;
+	uint16_t vbe_mode;
+	uint16_t vbe_interface_seg;
+	uint16_t vbe_interface_off;
+	uint16_t vbe_interface_len;
+} __attribute__((packed));
